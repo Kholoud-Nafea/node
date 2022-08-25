b3091d04a797c4bc164841834d3e49de1a73b2b9
Kholoud-Nafea <kholoudnafea74.com>
2022-08-25 18:19:42 +0200

    node

--- index.js
+++ index.js
@@ -2,48 +2,49 @@ const client = require('./connection.js')
 const express = require('express');
 const app = express();
 
-app.listen(3300, ()=>{
+app.listen(3300, () => {
     console.log("Sever is now listening at port 3300");
 })
 
 client.connect();
 
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 
-app.get('/users', (req, res)=>{
-    client.query(`Select * from users`, (err, result)=>{
-        if(!err){
+app.get('/users', (req, res) => {
+    client.query(`Select * from users`, (err, result) => {
+        if (!err) {
             res.send(result.rows);
         }
     });
     client.end;
 })
 
-app.get('/users/:id', (req, res)=>{
-    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-        if(!err){
+app.get('/users/:id', (req, res) => {
+    client.query(`Select * from users where id=${req.params.id}`, (err, result) => {
+        if (!err) {
             res.send(result.rows);
         }
     });
     client.end;
 })
 
-app.post('/user', (req, res)=> {
+app.post('/user', (req, res) => {
     const user = req.body;
     let insertQuery = `insert into users(id, name, email, password) 
                        values(${user.id}, '${user.name}', '${user.email}', '${user.password}')`
 
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
+    client.query(insertQuery, (err, result) => {
+        if (!err) {
             res.send('Insertion was successful')
         }
-        else{ console.log(err.message) }
+        else { console.log(err.message) }
     })
     client.end;
 })
 
-app.put('/user/:id', (req, res)=> {
+app.put('/user/:id', (req, res) => {
     const user = req.body
     const updateQuery = `update users
     set name = '${user.name}',
@@ -51,8 +52,8 @@ app.put('/user/:id', (req, res)=> {
     password = '${user.password}'
     where id = ${user.id}`
 
-    client.query(updateQuery, (err, result)=>{
-        if(!err) {
+    client.query(updateQuery, (err, result) => {
+        if (!err) {
             res.send('user updated successfully')
         } else {
             res.send(err.message)
@@ -64,7 +65,7 @@ app.put('/user/:id', (req, res)=> {
 app.delete('/user/:id', (req, res) => {
     let deleteQuery = `delete from users where id=${req.params.id}`
     client.query(deleteQuery, (err, result) => {
-        if(!err){
+        if (!err) {
             res.status(200).send('user deleted successfully')
         } else {
             res.send(err.message)
